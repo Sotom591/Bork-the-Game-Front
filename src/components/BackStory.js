ebd6a2549ff34b67e0355609832faa088d13416c
github username <github email address>
2019-01-24 01:26:14 -0500

    styling changes

--- src/components/BackStory.js
+++ src/components/BackStory.js
@@ -7,7 +7,7 @@ const BackStory = () =>{
     <div className="ui raised very padded text container segment">
       <p>Your story begins with you as Bork, the neighborhood doggo. It was a regular day as you strolled the town looking for your next meal. Suddenly, a few shops over, a restaurant door swings open and a crowd of screaming humans run out. </p>
        <p>Maybe they were celebrating because everything tasted so good?</p>
-      <p> Well, since no pup should never waste a perfect opportunity for a taste test, you speed up towards the restaurant. But, as you enter, you notice something about the food seems a little... off. </p>
+      <p> Well, since no pup should ever waste a perfect opportunity for a taste test, you speed up towards the restaurant. But, as you enter, you notice something about the food seems a little... off. </p>
     </div>
     <div className="storyBtns">
        <Link to={'/'}>
