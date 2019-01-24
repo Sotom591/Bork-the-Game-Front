31d86636dc872853a39bb5e40fa16490f543d983
github username <github email address>
2019-01-24 01:42:59 -0500

    styling additions to homepage

--- src/containers/HomeCont.js
+++ src/containers/HomeCont.js
@@ -5,7 +5,7 @@ const HomeCont = () => {
   return(
     <div className="home-cont">
     <p className="title">BORK </p>
-    <img alt="background picture" className="home-img" src="https://i.imgur.com/EoRe15h.png"/>
+    <img alt="background picture" className="home-img" src="https://i.imgur.com/OBaaIX4.png"/>
       <StartScreen />
     </div>
   )
