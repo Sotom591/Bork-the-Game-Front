d3c664ac112ef18062a32618c28534f9779a88d8
github username <github email address>
2019-01-24 11:30:25 -0500

    perf home background

--- src/containers/HomeCont.js
+++ src/containers/HomeCont.js
@@ -5,7 +5,8 @@ const HomeCont = () => {
   return(
     <div className="home-cont">
     <p className="title">BORK </p>
-    <img alt="background picture" className="home-img" src="https://i.imgur.com/OBaaIX4.png"/>
+    <img alt="background picture" className="home-img" src="https://i.imgur.com/xnhKU0s.png"/>
+    <img alt="doggo" className="home-doggo" src="https://i.imgur.com/OYq9koA.png"/>
       <StartScreen />
     </div>
   )
